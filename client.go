5d0b6f5a7a7fe4bfe4c110e1ad74e0954edac35d
Mark-Johannes Becker <mjb@marb.ec>
2015-10-06 17:53:04 +0200

    First version added

---
+++ client.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"log"
+	"os"
+	"path"
+)
+
+type DockerDeployClient struct {
+	Mode             string
+	SSHPort          int
+	SSHUser          string
+	SSHHost          string
+	SSHPassword      string
+	ProjectName      string
+	ComposeFile      string
+	StartTime        int
+	RemoteWorkingDir string
+	LocalWorkingDir  string
+	LocalArtifact    string
+	config           *ssh.ClientConfig
+	sshClient        *ssh.Client
+}
+
+func (c *DockerDeployClient) connect() error {
+	c.config = &ssh.ClientConfig{
+		User: c.SSHUser,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(c.SSHPassword),
+		},
+	}
+
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", c.SSHHost, c.SSHPort), c.config)
+	if err != nil {
+		return err
+	}
+	c.sshClient = client
+	return nil
+}
+
+func (c *DockerDeployClient) disconnect() error {
+	return c.sshClient.Close()
+}
+
+func (c *DockerDeployClient) executeCommand(command string) (string, error) {
+	session, err := c.sshClient.NewSession()
+	if err != nil {
+		return "", err
+	}
+	defer session.Close()
+
+	var b bytes.Buffer
+	session.Stdout = &b
+	if err := session.Run(command); err != nil {
+		return "", err
+	}
+	return b.String(), nil
+}
+
+func (c *DockerDeployClient) findLocalArtifact() error {
+	if _, err := os.Stat(c.LocalWorkingDir); err != nil {
+		return errors.New(fmt.Sprintf("Local working directory \"%v\" does not exist!", c.LocalWorkingDir))
+	}
+
+	d, err := os.Open(c.LocalWorkingDir)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Could not open local working directory: %v", err.Error()))
+	}
+	defer d.Close()
+
+	files, err := d.Readdir(-1)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if file.Mode().IsRegular() {
+			fpath := path.Join(c.LocalWorkingDir, file.Name())
+			if path.Ext(fpath) == ".zip" {
+				c.LocalArtifact = fpath
+				log.Printf("Local artifact found: %v", fpath)
+				return nil
+			}
+		}
+	}
+	return errors.New(fmt.Sprintf("Could not find local artifact in working directory %v", c.LocalWorkingDir))
+}
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+
+	c := new(DockerDeployClient)
+
+	flag.StringVar(&c.Mode, "mode", "DEPLOY,TEST", "Comma separated list of operations that should be executed. The following commands are available: CLEAR, DEPLOY, TEST, REMOVE.")
+	flag.IntVar(&c.SSHPort, "sshPort", 22, "Port for SSH authentication")
+	flag.StringVar(&c.SSHUser, "sshUser", "", "Username for SSH authentication")
+	flag.StringVar(&c.SSHPassword, "sshPassword", "", "Password for SSH authentication")
+	flag.StringVar(&c.SSHHost, "sshHost", "", "Hostname or IP of deployment target")
+	flag.StringVar(&c.ProjectName, "projectName", "", "Project name of Docker composition")
+	flag.StringVar(&c.ComposeFile, "composeFile", "docker-compose.yml", "Name of Docker Compose file")
+	flag.IntVar(&c.StartTime, "startTime", 10, "Time in seconds the deployment scripts waits for the containers to start")
+	flag.StringVar(&c.RemoteWorkingDir, "remoteWorkingDir", "", "Remote working directory, where the build artifact is copied and executed")
+	flag.StringVar(&c.LocalWorkingDir, "localWorkingDir", "", "Local working directory, where the build artifact is stored")
+	flag.Parse()
+
+	var err error
+
+	if err = c.findLocalArtifact(); err != nil {
+		log.Fatal(err)
+	}
+	if err = c.connect(); err != nil {
+		log.Fatal(err)
+	}
+	if err = c.disconnect(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Test...")
+
+}
+
+// TODO(mjb): Make sure folder exist, change folder, Clean working directory
+// TODO(mjb): Copy, unzip, delete artifact
+// TODO(mjb): Compose stop, clear
+// TODO(mjb): Compose build, run
+// TODO(mjb): Test, Service Discovery
+// TODO(mjb): Remove working dir, cleanup, disconnect
+// TODO(mjb): Travis Build
